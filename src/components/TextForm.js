e117d261c40e3ed6a832e3430e0a3c71c4fc52bf
DilipGelot <dilipgelot16@gmail.com>
2023-04-06 13:12:46 +0530

    new alert functions are added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,14 +5,17 @@ export default function TextForm(props) {
     const handleUpClick =() =>{
         let newText= text.toUpperCase();
         setText(newText)
+        props.showAlert("Converted to uppercase","success");
     }
     const handleLoClick =() =>{
         let newText= text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to lowercase","success");
     }
     const handleClearClick =() =>{
         let newText= '';
         setText(newText)
+        props.showAlert("Your text are cleared","warning");
     }
     const handleOnChange =(event) =>{
         setText(event.target.value)
@@ -21,10 +24,12 @@ export default function TextForm(props) {
         var text=document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied to clipboard","success");
     }
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert("Extra spaces are removed","success");
     }
     const [text, setText] = useState('');
     return (
